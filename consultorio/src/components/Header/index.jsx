76ec41cc02707f2c89072730687476b248f9a651
allen-xavier <allengx@msn.com>
2020-10-23 16:52:11 -0300

    header

---
+++ consultorio/src/components/Header/index.jsx
@@ -0,0 +1,32 @@
+import React from "react"
+import "./Header.css"
+import img from "../../asset/denteok.png"
+
+const Header = () => {
+    return (
+        <>
+            <header className="Header">
+                <div className="row">
+                    <div className="column2">
+                        <div className="column">
+                            <img className="icone" src={img} alt="" />
+                        </div>
+                        <div className="column">
+                            <h2>Clínica Dentária</h2>
+                        </div>
+                    </div>
+                
+
+                <div className="column nav">Tratamentos</div>
+                <div className="column nav">Sobre</div>
+                <div className="column nav">Localização</div>
+                </div>
+                {/* <li className="menu-item"><Link to= "/produto">Produto</Link></li> */}
+        </header>
+
+
+        </>
+    )
+}
+
+export default Header
---
+++ consultorio/src/views/Home/index.jsx
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from "react"
+import Header from "../../components/Header"
+import "./Home.css"
+
+const Home = () => {
+
+    return(
+        <main className="Home">
+            <Header nome="Clínica Dentária" />
+        </main>
+    )
+}
+export default Home
